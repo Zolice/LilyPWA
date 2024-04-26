4777265f46b3a78b562821f993eb7b624b84ec7f
Yong Zhen Long <vividzhenlong@gmail.com>
2024-04-26 11:57:32 +0800

    Fixed TextArea Placement

--- components/MarkdownComponent.vue
+++ components/MarkdownComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-row gap-1">
-        <img :src="$attrs.icon" alt="profile" class="rounded-full w-10 h-10 mx-2">
+    <div class="flex flex-row gap-1 my-4 px-4 w-full xl:w-4/5 mx-auto">
+        <img :src="$attrs.icon" alt="profile" class="rounded-full w-10 h-10 mr-2">
         <div class="flex flex-col">
             <p class="text-lg font-bold">{{$attrs.name}}</p>
             <p class="">{{ $attrs.text }}</p>
--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,22 @@
                 <p class="w-full my-auto">Search History</p>
                 <span class="material-symbols-outlined ">close</span>
             </div>
-            <div class="h-full">
+            <div class="h-full overflow-y-auto">
                 <ul class="space-y-2">
                     <li class="btn btn-ghost w-full hover:bg-[#FFC989]">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
+                    <li class="btn btn-ghost w-full">Home</li>
                     <li class="btn btn-ghost w-full">Home</li>
                 </ul>
             </div>
@@ -25,43 +38,36 @@
 
             </div>
         </div>
-        <div class="bg-[#1E1E1E] w-full h-full flex flex-col">
+        <div class="bg-[#1E1E1E] w-full h-screen flex flex-col">
             <HeaderComponent />
-            <div class="flex flex-col h-full mx-auto xl:w-4/5">
-                <div class="flex flex-col gap-y-4 overflow-y-auto h-full">
-                    <MarkdownComponent name="User" icon="/LilyGPT.png" text="test" />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                    <MarkdownComponent name="User" icon="/LilyGPT.png"
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
-                </div>
-                <div class="m-2 mx-auto flex flex-row items-center border-2 border-[#555] rounded-lg w-full">
-                    <textarea placeholder="Don't be shy, ask meow about anything!"
-                        class="w-full p-4 bg-transparent h-16" v-model="query" />
-                    <div class="btn btn-ghost">
-                        <span class="material-symbols-outlined">upload</span>
-                    </div>
-                    <div class="btn btn-ghost">
-                        <span class="material-symbols-outlined text-[#FFC989]">send</span>
-                    </div>
-                </div>
-
+            <div class="overflow-y-auto h-full mx-auto">
+                <MarkdownComponent name="User" icon="/LilyGPT.png" text="test" />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
+                <MarkdownComponent name="User" icon="/LilyGPT.png"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at ex in leo auctor laoreet. Nulla tempor nibh quis dictum volutpat. Etiam a ullamcorper est. Nulla libero massa, dapibus vitae ultricies vulputate, egestas eget purus. Donec tempor fringilla blandit. Suspendisse a rhoncus enim, tincidunt tempor tortor. Pellentesque elementum mauris a ante pulvinar tincidunt eget id nisl. Nam ac risus et urna euismod aliquam a nec metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat ipsum risus, nec pharetra tellus tempor ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus fermentum, nunc dapibus dapibus dictum, velit magna commodo diam, id sagittis velit elit a nisi. Pellentesque molestie vestibulum odio ut sodales. Integer aliquam non purus ut mollis. " />
             </div>
 
+            <div class="m-2 mx-auto flex flex-row items-center border-2 border-[#555] rounded-lg w-full xl:w-4/5">
+                <textarea placeholder="Don't be shy, ask meow about anything!" class="w-full p-4 bg-transparent h-16"
+                    v-model="query" />
+                <div class="btn btn-ghost">
+                    <span class="material-symbols-outlined">upload</span>
+                </div>
+                <div class="btn btn-ghost">
+                    <span class="material-symbols-outlined text-[#FFC989]">send</span>
+                </div>
+            </div>
         </div>
     </div>
-
-
-
-
 </template>
 
 <script setup lang="ts">
